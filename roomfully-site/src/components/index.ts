4635ac9bc89432f3c3266f3fb5150330071e6d7f
gnasamx <gnasamx@gmail.com>
2021-07-10 21:32:26 +0530

    Added inspiraton cards

--- roomfully-site/src/components/index.ts
+++ roomfully-site/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as CountryCard } from './country-card';
 export { default as ErrorFallback } from './error-fallback';
+export { default as InspirationForYourNextTrip } from './inspiration-for-your-next-trip';
 export { default as PageTitle } from './page-title';
 export { default as PropertyCard } from './property-card';
 export { default as PropertyTypeCard } from './property-type-card';
---
+++ roomfully-site/src/pages/ui-kit/component-configs/inspiration-for-your-next-trip.ts
@@ -0,0 +1,53 @@
+const InspirationForYourNextTrip = {
+  name: 'Inspiration For Your Next Trip',
+  examples: [
+    {
+      label: 'Default',
+      props: {
+        header: 'Get inspiration from your next trip',
+        description:
+          'Travel-themed Online Experiences from around the world. Every Airbnb Plus home is one-of-a-kind, thoughtfully designed, and equipped with a standard set of amenities – whether you’re in a private room or have the entire place to yourself.',
+        inspirations: [
+          {
+            image: {
+              src: 'https://bit.ly/3hY5lpp',
+              alt: 'Batumi city',
+            },
+            name: 'Batumi',
+            averagePrice: 40,
+            url: '#',
+          },
+          {
+            image: {
+              src: 'https://bit.ly/3hrr1LH',
+              alt: 'Ambrolauri city',
+            },
+            name: 'Ambrolauri',
+            averagePrice: 29,
+            url: '#',
+          },
+          {
+            image: {
+              src: 'https://bit.ly/3e4k80Q',
+              alt: 'Sighnaghi city',
+            },
+            name: 'Sighnaghi',
+            averagePrice: 20,
+            url: '#',
+          },
+          {
+            image: {
+              src: 'https://bit.ly/2UBH4hc',
+              alt: 'Tbilisi town',
+            },
+            name: 'Tbilisi',
+            averagePrice: 35,
+            url: '#',
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default InspirationForYourNextTrip;
