413b8618bdd0fb75436d1cedafb748ad2ba91f7d
gnasamx <gnasamx@gmail.com>
2021-07-10 23:04:48 +0530

    Added link overlay as reachlink

--- roomfully-site/src/components/property-card.tsx
+++ roomfully-site/src/components/property-card.tsx
@@ -1,6 +1,16 @@
-import { Box, Flex, HStack, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  HStack,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
 import * as React from 'react';
 import { HiStar } from 'react-icons/hi';
+import { Link as ReachLink } from 'react-router-dom';
 
 interface PropertyCardProps {
   image: {
@@ -16,52 +26,66 @@ interface PropertyCardProps {
 }
 
 const PropertyCard: React.FC<PropertyCardProps> = React.memo(
-  ({ image, name, propertyType, price, reviewPerMonth, numberOfReviews }) => {
+  ({
+    image,
+    name,
+    propertyType,
+    price,
+    reviewPerMonth,
+    numberOfReviews,
+    listingUrl,
+  }) => {
     return (
-      <Flex flexDirection='column' borderRadius='xl' overflow='hidden'>
-        <Image
-          src={image.src}
-          alt={image.alt}
-          objectFit='cover'
-          objectPosition='center'
-          loading='lazy'
-        />
-        <Box
-          padding={5}
-          alignItems='center'
-          justifyContent='space-between'
-          borderColor='gray.200'
-          borderLeftWidth={1}
-          borderRightWidth={1}
-          borderBottomWidth={1}
-          borderBottomRadius='xl'
-          width='full'
-          height='full'
-        >
-          <Box mb={4}>
-            <Text fontWeight='semibold'>{name}</Text>
-            <Text fontSize='sm' color='gray.600'>
-              {propertyType}
-            </Text>
-          </Box>
-          <SimpleGrid
-            templateColumns='1fr auto'
-            columnGap={4}
+      <LinkBox>
+        <Flex flexDirection='column' borderRadius='xl' overflow='hidden'>
+          <Image
+            src={image.src}
+            alt={image.alt}
+            objectFit='cover'
+            objectPosition='center'
+            loading='lazy'
+          />
+          <Box
+            padding={5}
             alignItems='center'
+            justifyContent='space-between'
+            borderColor='gray.200'
+            borderLeftWidth={1}
+            borderRightWidth={1}
+            borderBottomWidth={1}
+            borderBottomRadius='xl'
+            width='full'
+            height='full'
           >
-            <Text fontWeight='semibold'>{price} / night</Text>
-            <HStack spacing={1}>
-              <HiStar color='tomato' />
-              <Text fontWeight='semibold' fontSize='sm'>
-                {reviewPerMonth}
+            <Box mb={4}>
+              <Text fontWeight='semibold'>
+                <LinkOverlay as={ReachLink} to={listingUrl}>
+                  {name}
+                </LinkOverlay>
               </Text>
               <Text fontSize='sm' color='gray.600'>
-                ({numberOfReviews})
+                {propertyType}
               </Text>
-            </HStack>
-          </SimpleGrid>
-        </Box>
-      </Flex>
+            </Box>
+            <SimpleGrid
+              templateColumns='1fr auto'
+              columnGap={4}
+              alignItems='center'
+            >
+              <Text fontWeight='semibold'>{price} / night</Text>
+              <HStack spacing={1}>
+                <HiStar color='tomato' />
+                <Text fontWeight='semibold' fontSize='sm'>
+                  {reviewPerMonth}
+                </Text>
+                <Text fontSize='sm' color='gray.600'>
+                  ({numberOfReviews})
+                </Text>
+              </HStack>
+            </SimpleGrid>
+          </Box>
+        </Flex>
+      </LinkBox>
     );
   }
 );
