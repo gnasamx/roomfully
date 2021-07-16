af787e023aff33fd106fcbe56e17c3a034c3d429
gnasamx <gnasamx@gmail.com>
2021-07-16 10:25:53 +0530

    Sentiment sechma is ready to use

---
+++ roomfully-site/src/components/review-sentiment.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ReviewSentiment = React.memo(() => {
+  return <p>ReviewSentiment</p>;
+});
+
+export default ReviewSentiment;
---
+++ roomfully-site/src/models/review-sentiment.ts
@@ -0,0 +1,5 @@
+export interface ReviewSentiment {
+  type: string;
+  category: string;
+  value: number;
+}
