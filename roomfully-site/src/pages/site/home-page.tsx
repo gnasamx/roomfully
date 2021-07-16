8d28f47f62d8bce030dbab0cc1574014e335e2d2
gnasamx <gnasamx@gmail.com>
2021-07-16 17:21:02 +0530

    Clean homepage

--- roomfully-site/src/pages/site/home-page.tsx
+++ roomfully-site/src/pages/site/home-page.tsx
@@ -1,48 +1,13 @@
-import { Box, Container, Grid, Link } from '@chakra-ui/react';
+import { Container, Link } from '@chakra-ui/react';
 import React from 'react';
 import { Link as ReachLink } from 'react-router-dom';
-import { ListingDescriptionReviews } from '../../components';
 
 const HomePage = () => {
-  const props = {
-    reviewsScorePerMonth: 4.96,
-    numberOfReviews: 235,
-    allReviewsButton: 'All Reviews',
-    reviewSentiments: [
-      {
-        type: 'Positive',
-        category: '4 stars and above',
-        value: 80,
-      },
-      {
-        type: 'Neutral',
-        category: '3 stars and above',
-        value: 15,
-      },
-      {
-        type: 'Negative',
-        category: 'under 2 stars',
-        value: 5,
-      },
-    ],
-  };
-
   return (
     <Container as='section' maxWidth='container.xl' padding={4}>
       <Link as={ReachLink} to='/ui-kit'>
         Go to Ui-Kit
       </Link>
-      <Grid templateColumns='1fr 350px' columnGap={6}>
-        <ListingDescriptionReviews {...props} />
-        <Box
-          borderColor='gray.200'
-          borderWidth={1}
-          borderRadius='lg'
-          padding={4}
-        >
-          This is a box
-        </Box>
-      </Grid>
     </Container>
   );
 };
